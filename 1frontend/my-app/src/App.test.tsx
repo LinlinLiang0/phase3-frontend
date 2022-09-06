f4158da952a839459e17966fb8c68a0284a4ea69
LinlinLiang0 <llia618@aucklanduni.ac.nz>
2022-09-06 21:40:06 +0800

    change title, and change modify default test

--- 1frontend/my-app/src/App.test.tsx
+++ 1frontend/my-app/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Movie Search', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Movie Search/i);
   expect(linkElement).toBeInTheDocument();
 });
